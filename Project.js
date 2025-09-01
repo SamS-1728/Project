7b11cb4b51330661320aa156fbce4cf7b59a6173
Samuel Shelton <samshelton@Samuels-MacBook-Air.local>
2025-09-01 16:49:34 +0100

    1st cycle

--- Project.js
+++ Project.js
@@ -9,6 +9,7 @@ const available_algorithms = [
   "sha256",
 ];
 
+/* Button colors*/
 const cipherButtons = [
   ["caesar", "#B2F2B2"],
   ["substitution", "#C8E8A0"],
@@ -21,9 +22,9 @@ const cipherButtons = [
 ];
 
 const sections = ["title", "history", "explanation", "ciphertext"];
-
 var selected_algorithms = [];
 
+/* Runs correct setup for that page */
 document.addEventListener("DOMContentLoaded", function () {
   const page = document.body.id;
   if (page == "initial_page") {
@@ -33,11 +34,13 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+/* Sets up initial page */
 function initial_page_setup() {
   /*Apply button colors*/
   for (const [button, color] of cipherButtons) {
     document.getElementById(button).style.backgroundColor = color;
   }
+
   /* Dragging functionality */
   for (const algo of available_algorithms) {
     const element = document.getElementById(algo);
@@ -47,30 +50,33 @@ function initial_page_setup() {
   }
 
   const drop = document.getElementById("drop_target");
-
   drop.addEventListener("dragover", function (e) {
     e.preventDefault();
   });
 
+  /* Drop functionality */
   drop.addEventListener("drop", function (e) {
     e.preventDefault();
     const buttonId = e.dataTransfer.getData("text/plain");
     const button = document.getElementById(buttonId);
     const parent_div = document.getElementById("drop_area");
 
+    /* Adds input section and displays both */
     button.style.width = "40%";
     const input = document.getElementById(buttonId + "_inputs");
     input.style.display = "flex";
     input.style.backgroundColor = cipherButtons.find((item) => item[0] === buttonId)[1];
     parent_div.insertBefore(button, drop);
+    button.draggable = false;
     parent_div.insertBefore(input, drop);
 
+    /* Selects algorithms and ensures 2-4 are selected */
     selected_algorithms.push(buttonId);
     if (selected_algorithms.length == 2) {
       submitter = document.getElementById("submit");
       submitter.style.display = "flex";
-      note = document.getElementById("submit_note");
-      note.style.display = "flex";
+      container = document.getElementById("submit_container");
+      container.style.display = "flex";
     }
     if (selected_algorithms.length == 4) {
       drop.style.display = "none";
@@ -86,31 +92,201 @@ function random_text() {
 }
 function extract(text) {
   start = Math.floor(Math.random() * 5200000);
-  text = text.slice(start, start + 2000);
+  text = text.slice(start, start + 1250);
   text = text.slice(text.indexOf("\n") + 1, text.lastIndexOf("\n"));
   text = text.replace(/\s+/g, " ");
   document.getElementById("input_text").value = text.slice(1);
 }
 
+/* Handles the submission of the initial page */
 function initial_submit() {
   sessionStorage.setItem(
     "selected_algorithms",
     JSON.stringify(selected_algorithms)
   );
+  /* Stores all inputs */
+  var input_text = document.getElementById("input_text").value;
+  sessionStorage.setItem("input_text", input_text);
+  /* Each case stores the inputs for that specific algorithm in session storage */
+  for (const algo of selected_algorithms) {
+    switch (algo) {
+      case "caesar":
+        var caesar_shift = document.getElementById("caesar_shift").value;
+        sessionStorage.setItem("caesar_shift", caesar_shift);
+        general_substitution_inputs("caesar");
+      case "substitution":
+        var substitution_keyword = document.getElementById("substitution_keyword").value;
+        sessionStorage.setItem("substitution_keyword", substitution_keyword);
+        general_substitution_inputs("substitution");
+      case "affine":
+        var affine_a = document.getElementById("affine_a").value;
+        sessionStorage.setItem("affine_a", affine_a);
+        var affine_b = document.getElementById("affine_b").value;
+        sessionStorage.setItem("affine_b", affine_b);
+        general_substitution_inputs("affine");
+    }
+  }
 }
 
-/* Initial page submission handling */
+/* Stores the punctuation, space and case inputs that are repeated across general substitution ciphers */
+function general_substitution_inputs(algo) {
+  var spaces = document.getElementById(algo + "_spaces").checked;
+  sessionStorage.setItem(algo + "_spaces", spaces);
+  var punctuation = document.getElementById(algo + "_punctuation").checked;
+  sessionStorage.setItem(algo + "_punctuation", punctuation);
+  var char_case = document.getElementById(algo + "_case").checked;
+  sessionStorage.setItem(algo + "_case", char_case);
+}
+
+/* Sets up the final page */
 function final_page_setup() {
   selected_algorithms = JSON.parse(
     sessionStorage.getItem("selected_algorithms")
   );
+  /* Runs each algorithm's function */
+  for (const algo of selected_algorithms) {
+    switch (algo) {
+      case "caesar":
+        caesar();
+        break
+      case "substitution":
+        substitution();
+        break;
+      case "affine":
+        affine();
+        break;
+    }
+  }
+
+  /* Displays results page dependent on algorithm selection (choices and number selected) */
   for (const algo of selected_algorithms) {
     const result = document.getElementById(algo + "_result");
     result.style.display = "flex";
-    result.style.width = 100 / selected_algorithms.length-1 + "%";
+    result.style.width = 100 / selected_algorithms.length - 1 + "%";
     for (const section of sections) {
       var section_id = document.getElementById(algo + "_" + section);
       section_id.style.backgroundColor = cipherButtons.find((item) => item[0] === algo)[1];
+    }
+  }
+  for (const section of sections) {
+    var section_heights = [];
+    for (const algo of selected_algorithms) {
+      var section_id = document.getElementById(algo + "_" + section);
+      section_heights.push(section_id.offsetHeight);
+    }
+    new_height = Math.max(...section_heights)-8;
+    console.log(section_heights)
+    console.log(new_height)
+    for (const algo of selected_algorithms) {
+      var section_id = document.getElementById(algo + "_" + section);
+      section_id.style.minHeight = new_height+"px";
+      console.log(section_id.offsetHeight);
+    }
   }
 }
+
+/* The following functions implement each algorithm and insert these into the results page */
+function caesar() {
+  var input_text = sessionStorage.getItem("input_text");
+  input_text = general_substitution_inputs_handling("caesar", input_text);
+  var shift = parseInt(sessionStorage.getItem("caesar_shift")) % 26;
+  var ciphertext = "";
+  for (var i = 0; i < input_text.length; i++) {
+    var value = input_text.charCodeAt(i);
+    if (value >= 65 && value <= 90) {
+      if (value + shift > 90) {
+        value += shift - 26;
+      } else {
+        value += shift;
+      }
+      ciphertext += String.fromCharCode(value);
+    } else if (value >= 97 && value <= 122) {
+      if (value + shift > 122) {
+        value += shift - 26;
+      } else {
+        value += shift;
+      }
+      ciphertext += String.fromCharCode(value);
+    } else {
+      ciphertext += input_text[i];
+    }
+  }
+  document.getElementById("caesar_ciphertext_content").innerHTML += `<p>${ciphertext}</p>`;
+}
+
+function substitution() {
+  var input_text = sessionStorage.getItem("input_text");
+  input_text = general_substitution_inputs_handling("substitution", input_text);
+  console.log(input_text)
+  var keyword = sessionStorage.getItem("substitution_keyword").toUpperCase();
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var substitution_alphabet = "";
+  for (var i = 0; i < keyword.length; i++) {
+    if (alphabet.includes(keyword[i])) {
+      substitution_alphabet += keyword[i];
+      alphabet = alphabet.replace(keyword[i], "");
+    }
+  }
+  substitution_alphabet += alphabet;
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  substitution_alphabet += substitution_alphabet.toLowerCase();
+  console.log(substitution_alphabet)
+  console.log(alphabet)
+  var ciphertext = "";
+  for (var i = 0; i < input_text.length; i++) {
+    if (alphabet.includes(input_text[i])) {
+      ciphertext += substitution_alphabet[alphabet.indexOf(input_text[i])];
+    } else {
+      ciphertext += input_text[i];
+    }
+  }
+  document.getElementById("substitution_ciphertext_content").innerHTML += `<p>${ciphertext}</p>`;
+}
+
+function affine() {
+  var input_text = sessionStorage.getItem("input_text");
+  input_text = general_substitution_inputs_handling("affine", input_text);
+  var a = parseInt(sessionStorage.getItem("affine_a"));
+  var b = parseInt(sessionStorage.getItem("affine_b"));
+  var ciphertext = "";
+  for (var i = 0; i < input_text.length; i++) {
+    var value = input_text.charCodeAt(i);
+    if (value >= 65 && value <= 90) {
+      value -= 64;
+      value = (value * a + b) % 26;
+      if (value == 0) {
+        value = 26;
+      }
+      ciphertext += String.fromCharCode(value + 64);
+    } else if (value >= 97 && value <= 122) {
+      value -= 96;
+      value = (value * a + b) % 26;
+      if (value == 0) {
+        value = 26;
+      }
+      ciphertext += String.fromCharCode(value + 96);
+    } else {
+      ciphertext += input_text[i];
+    }
+  }
+  document.getElementById("affine_ciphertext_content").innerHTML += `<p>${ciphertext}</p>`;
+}
+
+/* Acts on the inputs that are used across the general substitution ciphers */
+function general_substitution_inputs_handling(algo, input_text) {
+  var spaces = sessionStorage.getItem(algo + "_spaces");
+  var punctuation = sessionStorage.getItem(algo + "_punctuation");
+  var char_case = sessionStorage.getItem(algo + "_case");
+
+  if (spaces == "true") {
+    input_text = input_text.replaceAll(" ", "");
+    input_text = input_text.replace(new RegExp(`.{${5}}`, 'g'), '$&' + ' ');
+  }
+  if (punctuation == "true") {
+    input_text = input_text.replace(/[.,\/#!?'$"%\^&\*;:{}=\-_`~()]/g, "");
+  }
+  if (char_case == "true") {
+    input_text = input_text.toUpperCase();
+  }
+  return input_text;
 }
--- readme.md
+++ readme.md
@@ -9,3 +9,6 @@ Third commit - results page setup (accidentally missed from basic setup)
 Fourth commit - Iterative cycle 1 part 1 - initial page layout with drag and drop algorithm selection (with placeholder for additional inputs) and general data input (plus shakespearean text additional feature hence addition of text file). Submit takes 2-4 algorithms however only opens empty results page.
 
 Fifth commit - Iterative cycle 1 part 2 - results page barebones layout (with selected algorithms and appropriate colour schemes), individual algorithm logos added (AI generated), general improvements (font, input box style and colour etc).
+
+Sixth commit - Iterative cycle part 3 (final) - implementation of basic algorithms (caesar, substitution and affine) including all inputs and displaying ciphertext, general design refinements (e.g., submit button, result section sizes etc). 
+Notable ommissions after 1st cycle: validation, inputs for other algorithms, full results (beyond just ciphertext).
